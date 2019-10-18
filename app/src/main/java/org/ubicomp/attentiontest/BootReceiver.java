de66cad6cfc880184f5f22ecab74a047a1ee1c58
akuvisur <aku.visuri@gmail.com>
2019-10-18 10:09:16 +0300

    - Fixed? Automatical start after boot

---
+++ app/src/main/java/org/ubicomp/attentiontest/BootReceiver.java
@@ -0,0 +1,15 @@
+package org.ubicomp.attentiontest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent serviceIntent = new Intent(context, NotificationTriggerService.class);
+            context.startService(serviceIntent);
+        }
+    }
+}
